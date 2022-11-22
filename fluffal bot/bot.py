98790d08951fbf8bf37f194be25d0272dfe1d513
Lexy <93837875+lexyclear@users.noreply.github.com>
2022-11-22 00:12:05 -0600

    2022 updated
    updated

--- fluffal bot/bot.py
+++ fluffal bot/bot.py
@@ -1,35 +1,12 @@
 import discord
-import random
 from discord.ext import commands
-
+import random
 
 client = commands.Bot(command_prefix=">")
 
 @client.event 
 async def on_ready():
     print("Bot is ready")
-
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hi there!!!")
-
-@client.command()
-async def trans(ctx):
-    await ctx.send("hot bishes on the pink pills")
-
-
-@client.command()
-async def nini(ctx):
-    await ctx.send("night everyone")
-
-
-@client.command()
-async def sex(ctx):
-    await ctx.send("bonk")
-
-@client.command()
-async def supporter(ctx):
-    await ctx.send("If you wish to support bot development go help my mistress on kofi! https://ko-fi.com/doggo_vt")
 
 @client.event
 async def on_member_join(member):
@@ -39,9 +16,53 @@ async def on_member_join(member):
 async def  ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
+@client.command()
+async def birthday(ctx):
+    await ctx.send("Happy birthday!")
+
+@client.command()
+async def Bolshevism(ctx):
+    await ctx.send("Bolshevism (from Bolshevik) is a revolutionary socialist current of Soviet Marxist-Leninist political thought and political regime associated with the formation of a rigidly centralized, cohesive and disciplined party of social revolution, focused on overthrowing the existing capitalist state system, seizing power and establishing the dictatorship of the proletariat")
+
+@client.command()
+async def Geass(ctx):
+    await ctx.send("Code Geass is a good tv show.")
+
+@client.command()
+async def gm(ctx):
+    await ctx.send("good morning!")
+
+@client.command() 
+async def hello(ctx):
+    await ctx.send("Hi there!!!")
+
+@client.command()
+async def makeout(ctx):
+    await ctx.send("kisses you intensely")
+
+@client.command()
+async def mom(ctx):
+    await ctx.send(" is baking cookies.")
+
+@client.command()
+async def nini(ctx):
+    await ctx.send("night everyone")
+
+@client.command()
+async def proletariat(ctx):
+    await ctx.send("workers or working-class people, regarded collectively (often used with reference to Marxism).")
+
+@client.command()
+async def sex(ctx):
+    await ctx.send("bonk")
+
+@client.command() 
+async def trans(ctx):
+    await ctx.send("bishes on the pink pills")
+
 @client.command(aliases=['8ball'])
 async def _8ball(ctx,*,question):
-    responses = ['ID10T error','nowi said no','It is Certain.','It is decidedly so.','Without a doubt.',' Yes definitely.','You may accept it.','Most likely.','Yes.','Your family will hate you for this decision.','Better not tell you now.','A goldfish asks better questions then you.','Dont count on it.','No.','Your family will love you for this decision.','Im unsure.','perhaps.','ask again.']
+    responses = ['ID10T error','No you may not','Fuck yes','Fuck NO','God disagrees with you','God agrees with you','nowi said no','It is Certain.','It is decidedly so.','Without a doubt.',' Yes definitely.','You may accept it.','Most likely.','Yes.','Your family will hate you for this decision.','Better not tell you now.','A goldfish asks better questions then you.','Dont count on it.','No.','Your family will love you for this decision.','Im unsure.','perhaps.','ask again.']
     await ctx.send(f'question: {question}\nAnswer: {random.choice(responses)}')
 
 @client.command(aliases=['1d4'])
@@ -74,30 +95,9 @@ async def d1010(ctx,*,roll):
     responses = ['10','20','30','40','50','60','70','80','90','100']
     await ctx.send(f'roll: {roll}\nrolled: {random.choice(responses)}')
 
-@client.command()
-async def mom(ctx):
-    await ctx.send("Username.Username is baking cookies.")
+intents = discord.Intents.all()
+commands.Bot(command_prefix=">", intents=intents)
 
 bot = commands.Bot(command_prefix='>')
 
-@client.command()
-async def gm(ctx):
-    await ctx.send("good morning!")
-
-@client.command()
-async def birthday(ctx):
-    await ctx.send("Happy birthday!")
-
-
-@client.command()
-async def Bolshevism(ctx):
-    await ctx.send("Bolshevism (from Bolshevik) is a revolutionary socialist current of Soviet Marxist-Leninist political thought and political regime associated with the formation of a rigidly centralized, cohesive and disciplined party of social revolution, focused on overthrowing the existing capitalist state system, seizing power and establishing the dictatorship of the proletariat")
-
-@client.command()
-async def proletariat(ctx):
-    await ctx.send("workers or working-class people, regarded collectively (often used with reference to Marxism).")
-
-
-
-client.run(private key afd cog)
-
+client.run("private key")
